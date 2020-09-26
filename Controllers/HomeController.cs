ebd9ce771e4120ec3a378c037d0b95f1cc00cdbe
Quyen-nv <Quyen-nv@best-inc.com>
2020-09-26 09:25:43 +0700

    Second commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace CSharp.Controllers
         {
             return View();
         }
+        public IActionResult Promotion()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
